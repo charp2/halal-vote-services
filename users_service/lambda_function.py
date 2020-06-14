b1bfe2b27f80feaa3b68d8e9b69ed5b66fbcb341
Yahya Saad <yahya@ys-macbook-pro.fios-router.home>
2020-06-14 14:51:12 -0400

    lowercase sessiontoken

--- users_service/lambda_function.py
+++ users_service/lambda_function.py
@@ -60,7 +60,7 @@ def handler(event: eventType, context):
     requestHeaders = event['headers']
 
     if path not in no_session_token:
-        status_code, msg = valid_user(requestBody['username'], requestHeaders['sessionToken'], conn, logger)
+        status_code, msg = valid_user(requestBody['username'], requestHeaders['sessiontoken'], conn, logger)
 
         if status_code != 200:
             return {'statusCode': status_code, 'body': msg, 'headers': get_response_headers()}
