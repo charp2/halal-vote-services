5c2023b2fcbbaaaf03e9938600c0cbd5ff2bcade
Yahya Saad <yahya@ys-macbook-pro.fios-router.home>
2020-06-14 21:23:10 -0400

    update numReplies when comment is deleted

--- comments_service/delete_comment.py
+++ comments_service/delete_comment.py
@@ -1,5 +1,6 @@
 # standard python imports
 import pymysql
+import json
 
 # our imports
 from utils import generate_error_response
@@ -24,7 +25,7 @@ def delete_comment(data: dataType, conn, logger):
         if has_descendent(conn, id):
             with conn.cursor() as cur:
                 cur.execute(
-                    '''UPDATE Comments SET comment = "__DELETED__" WHERE id=%(id)s and username=%(username)s''',
+                    '''UPDATE Comments SET comment = "__deleted__" WHERE id=%(id)s and username=%(username)s''',
                     {'id': id, 'username': username}
                 )
                 if cur.rowcount <= 0:
@@ -32,7 +33,7 @@ def delete_comment(data: dataType, conn, logger):
 
                 conn.commit()
 
-            return generate_success_response("Updated comment with id: %s to reflect __DELETED__ status" %(id))
+            return generate_success_response(json.dumps({ "deletedId": id, "psuedoDelete": 1 }, default=str))
 
         else:
             with conn.cursor() as cur:
@@ -40,6 +41,12 @@ def delete_comment(data: dataType, conn, logger):
                     '''delete from Comments where id=%(id)s and username=%(username)s''', 
                     {'id': id, 'username': username}
                 )
+                if has_ancestor(conn, id):
+                    cur.execute(
+                        '''UPDATE Comments SET numReplies = numReplies - 1 
+                        WHERE id in (select ancestor from CommentsClosure where descendent=%(id)s and isDirect=1)''', #TODO: optimize this
+                        {'id': id}
+                    )
                 if cur.rowcount > 0:
                     cur.execute(
                         '''delete from CommentsClosure where descendent=%(id)s''',
@@ -50,7 +57,7 @@ def delete_comment(data: dataType, conn, logger):
 
                 conn.commit()
 
-            return generate_success_response("Deleted comment with id: %s" %(id))
+            return generate_success_response(json.dumps({ "deletedId": id, "psuedoDelete": 0 }, default=str))
                 
 
     except Exception as e:
@@ -59,6 +66,15 @@ def delete_comment(data: dataType, conn, logger):
 def has_descendent(conn, id: int):
     with conn.cursor() as cur:
         cur.execute('''SELECT EXISTS (SELECT * FROM CommentsClosure WHERE ancestor=%(id)s)''', 
+            {'id': id}
+        )
+        conn.commit()
+
+    return cur.fetchone()[0]
+
+def has_ancestor(conn, id: int):
+    with conn.cursor() as cur:
+        cur.execute('''SELECT EXISTS (SELECT * FROM CommentsClosure WHERE descendent=%(id)s)''', 
             {'id': id}
         )
         conn.commit()
