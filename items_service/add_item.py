73472ffa571a26ccbfe07c61f75fb91ac25aca91
AbdulBaseer Khan <baseer@AbdulBaseersMBP.fios-router.home>
2020-08-01 17:14:19 -0400

    Returned json object for add_item

--- items_service/add_item.py
+++ items_service/add_item.py
@@ -1,3 +1,6 @@
+# standard python imports
+import json
+
 # our imports
 from utils import generate_error_response
 from utils import generate_success_response
@@ -10,9 +13,13 @@ def add_item(data: dataType, conn, logger):
     # Access DB
     try:
         with conn.cursor() as cur:
-            cur.execute('insert into Items (itemName, username, halalVotes, haramVotes) values(%(itemName)s, %(username)s, 0, 0)', {'itemName': data['itemName'], 'username': data['username']})
-        conn.commit()
+            item_name = data['itemName']
+            username = data['username']
+
+            cur.execute('insert into Items (itemName, username, halalVotes, haramVotes) values(%(itemName)s, %(username)s, 0, 0)', {'itemName': item_name, 'username': username})
+            conn.commit()
+
+            return generate_success_response(json.dumps(item_name, default=str))
+
     except Exception as e:
         return generate_error_response(500, str(e))
-
-    return generate_success_response("Added Item '%s' into Items table" %(data['itemName']))
