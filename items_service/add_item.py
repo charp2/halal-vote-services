f995ea82b8769469e717dc9694dd307e3fb31f55
AbdulBaseer Khan <baseer@AbdulBaseersMBP.fios-router.home>
2020-08-22 15:59:21 -0400

    Changed add item to intake description and populate ItemDescriptions accordingly

--- items_service/add_item.py
+++ items_service/add_item.py
@@ -7,7 +7,8 @@ from utils import generate_success_response
 
 dataType = {
     "itemName": str,
-    "username": str
+    "username": str,
+    "description": str
 }
 def add_item(data: dataType, conn, logger):
     # Access DB
@@ -15,11 +16,24 @@ def add_item(data: dataType, conn, logger):
         with conn.cursor() as cur:
             item_name = data['itemName']
             username = data['username']
+            description = data['description']
 
-            cur.execute('insert into Items (itemName, username, halalPoints, haramPoints, numVotes) values(%(itemName)s, %(username)s, 0, 0, 0)', {'itemName': item_name, 'username': username})
+            cur.execute('select exists(select * from Items where itemName=%(itemName)s)', {'itemName': item_name})
             conn.commit()
 
-            return generate_success_response(item_name)
+            result = cur.fetchone()
+            if result:
+                if result[0] == 1:
+                    cur.execute('insert into ItemDescriptions (itemName, username, description) values(%(itemName)s, %(username)s, %(description)s) on duplicate key update description=%(description)s', {'itemName': item_name, 'username': username, 'description': description})
+                    conn.commit()
+                    return generate_success_response(item_name)
+                else:
+                    cur.execute('insert into Items (itemName, username, halalPoints, haramPoints, numVotes) values(%(itemName)s, %(username)s, 0, 0, 0)', {'itemName': item_name, 'username': username})
+                    cur.execute('insert into ItemDescriptions (itemName, username, description) values(%(itemName)s, %(username)s, %(description)s)', {'itemName': item_name, 'username': username, 'description': description})
+                    conn.commit()
+                    return generate_success_response(item_name)
+            else:
+                return generate_error_response(500, "Checking if item exists failed.")
 
     except Exception as e:
         return generate_error_response(500, str(e))
