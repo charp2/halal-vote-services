487d4dd55bc9564756f8ea2d3dae0db6efd0840c
AbdulBaseer Khan <baseer@AbdulBaseers-MacBook-Pro.local>
2020-11-05 22:33:41 -0500

    Get topic analytics adds up to final vote counts for any interval

--- topics_service/get_topic_analytics.py
+++ topics_service/get_topic_analytics.py
@@ -10,34 +10,34 @@ from datetime import timedelta
 
 dataType = {
     "topicTitle": str,
-    "prevType": str,
-    "numPrev": str
+    "interval": str,
+    "numIntervals": str
 }
 def get_topic_analytics(data: dataType, conn, logger):
     # Access DB
     try:
         with conn.cursor(pymysql.cursors.DictCursor) as cur:
             topic_title = data['topicTitle']
-            prev_type = data['prevType'].lower()
-            num_prev = data.get('numPrev', None)
+            interval = data['interval'].lower()
+            num_intervals = data.get('numIntervals', None)
             end_time = generate_timestamp()
 
-            if prev_type != "d" and prev_type != "w":
+            if interval != "d" and interval != "w":
                 return generate_error_response(400, "Previous type is invalid.")
 
-            if num_prev == None:
+            if num_intervals == None:
                 start_time = None
             else:
-                num_prev = int(num_prev)
-                halal_counts = [0] * num_prev
-                haram_counts = [0] * num_prev
+                num_intervals = int(num_intervals)
+                halal_counts = [None] * num_intervals
+                haram_counts = [None] * num_intervals
 
-                if prev_type == "d":
-                    start_time = end_time - timedelta(days=num_prev)
+                if interval == "d":
+                    start_time = end_time - timedelta(days=num_intervals)
                 else:
-                    start_time = end_time - timedelta(weeks=num_prev)
+                    start_time = end_time - timedelta(weeks=num_intervals)
 
-            query_string = 'select timeStamp, vote from UserTopicVotes where topicTitle=%(topicTitle)s'
+            query_string = 'select username, timeStamp, vote from UserTopicVotes where topicTitle=%(topicTitle)s'
             query_params = {'topicTitle': topic_title}
 
             if start_time != None:
@@ -45,31 +45,87 @@ def get_topic_analytics(data: dataType, conn, logger):
                 query_params['startTime'] = start_time
                 query_params['endTime'] = end_time
             
+            query_string += ' order by timeStamp ASC'
             cur.execute(query_string, query_params)
             conn.commit()
+            vote_results = cur.fetchall()
 
-            results = cur.fetchall()
+            cur.execute('''
+                select halalPoints, haramPoints from Topics
+                where topicTitle=%(topicTitle)s
+            ''', {'topicTitle': topic_title})
+            conn.commit()
+            final_vote_results = cur.fetchone()
 
-            if results and len(results) > 0:
-                if num_prev == None:
-                    start_time = results[len(results) - 1]["timeStamp"]
-                    num_prev = (end_time - start_time).days
+            final_halal_votes = 0
+            final_haram_votes = 0
 
-                    if prev_type == "w":
-                        num_prev = num_prev // 7
-                    
-                    num_prev += 1
-                    halal_counts = [0] * num_prev
-                    haram_counts = [0] * num_prev
+            if final_vote_results:
+                final_halal_votes = final_vote_results["halalPoints"]
+                final_haram_votes = final_vote_results["haramPoints"]
 
-                for value in results:
+            running_halal_votes = 0
+            running_haram_votes = 0
+
+            if vote_results and len(vote_results) > 0:
+                running_halal_voters = set()
+                running_haram_voters = set()
+
+                for value in vote_results:
+                    current_user = value["username"]
                     current_time = value["timeStamp"]
                     current_vote = value["vote"]
-                    diff = (end_time - current_time).days if prev_type == "d" else (end_time - current_time).days // 7
+                    diff = (end_time - current_time).days if interval == "d" else (end_time - current_time).days // 7
+
                     if current_vote > 0:
-                        halal_counts[num_prev - 1 - diff] += 1
+                        running_halal_votes += 1
+                        running_halal_voters.add(current_user)
+                        halal_counts[num_intervals - 1 - diff] = running_halal_votes
+
+                        if current_user in running_haram_voters:
+                            running_haram_votes -= 1
+                            running_haram_voters.remove(current_user)
+
+                            if haram_counts[num_intervals - 1 - diff] == None:
+                                haram_counts[num_intervals - 1 - diff] = -1
+                            else:
+                                haram_counts[num_intervals - 1 - diff] -= 1
+
                     elif current_vote < 0:
-                        haram_counts[num_prev - 1 - diff] += 1
+                        running_haram_votes += 1
+                        running_haram_voters.add(current_user)
+                        haram_counts[num_intervals - 1 - diff] = running_haram_votes
+
+                        if current_user in running_halal_voters:
+                            running_halal_votes -= 1
+                            running_halal_voters.remove(current_user)
+
+                            if halal_counts[num_intervals - 1 - diff] == None:
+                                halal_counts[num_intervals - 1 - diff] = -1
+                            else:
+                                halal_counts[num_intervals - 1 - diff] -= 1
+
+            halal_count_diff = final_halal_votes - running_halal_votes
+            haram_count_diff = final_haram_votes - running_haram_votes
+            last_halal_count = 0
+            last_haram_count = 0
+
+            for i in range(num_intervals):
+                if halal_counts[i] == None:
+                    halal_counts[i] = last_halal_count
+                else:
+                    if halal_counts[i] < 0:
+                        halal_counts[i] += last_halal_count
+                    last_halal_count = halal_counts[i]
+                halal_counts[i] += halal_count_diff
+
+                if haram_counts[i] == None:
+                    haram_counts[i] = last_haram_count
+                else:
+                    if haram_counts[i] < 0:
+                        haram_counts[i] += last_haram_count
+                    last_haram_count = haram_counts[i]
+                haram_counts[i] += haram_count_diff
 
             return generate_success_response({"halalCounts": halal_counts, "haramCounts": haram_counts})
 
