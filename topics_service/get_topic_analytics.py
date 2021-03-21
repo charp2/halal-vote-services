8517a3d61029bccc78eebae5c3b9714a9490d6a0
AbdulBaseer Khan <baseer@AbdulBaseers-MacBook-Pro-2.local>
2021-03-21 02:22:02 -0400

    Changed implementation of get_topic_analytics to not rely on halalPoints/haramPoints and added rollback logic to vote_topic

--- topics_service/get_topic_analytics.py
+++ topics_service/get_topic_analytics.py
@@ -21,114 +21,99 @@ def get_topic_analytics(data: dataType, conn, logger):
         with conn.cursor(pymysql.cursors.DictCursor) as cur:
             topic_title = data['topicTitle']
             interval = data['interval'].lower()
-            num_intervals = data.get('numIntervals', None)
+            requested_num_intervals = data.get('numIntervals', None)
             current_time = generate_timestamp()
             end_time = get_time_floor(current_time)
 
             if interval != "d" and interval != "w" and interval != "a":
                 return generate_error_response(400, "interval type is invalid")
 
-            if interval == "a":
-                cur.execute('''
-                    select voteTime from UserTopicVotes
-                    where topicTitle = %(topicTitle)s
-                    ORDER BY voteTime ASC LIMIT 1
-                ''', {'topicTitle': topic_title})
-                conn.commit()
-                start_time = cur.fetchone()
+            if (interval == "d" or interval == "w") and requested_num_intervals == None:
+                return generate_error_response(400, "numIntervals must be passed in for interval type d or w") 
 
-                if start_time:
-                    start_time = get_time_floor(start_time["voteTime"])
-                    num_intervals = (end_time - start_time).days + 1
+            cur.execute('''
+                select username, vote, voteTime from UserTopicVotes
+                where topicTitle=%(topicTitle)s
+                ORDER BY voteTime ASC
+            ''', {'topicTitle': topic_title})
+            vote_history = cur.fetchall()
 
-                    if num_intervals <= 365:
-                        interval = "d"
-                        num_intervals = max(7, num_intervals)
+            if vote_history and len(vote_history) > 0:
+                start_time = get_time_floor(vote_history[0]["voteTime"])
+                num_intervals = (end_time - start_time).days + 1
 
-                    else:
-                        interval = "w"
-                        num_intervals = math.ceil(num_intervals / 7)
+                if num_intervals <= 365:
+                    interval_used = "d"
+                    num_intervals = max(7, num_intervals)
 
                 else:
-                    response = {"interval": "d", "halalCounts": [0] * 7, "haramCounts": [0] * 7}
-                    return generate_success_response(response)
+                    interval_used = "w"
+                    num_intervals = math.ceil(num_intervals / 7)
 
-            elif num_intervals != None:
-                num_intervals = int(num_intervals)
+                requested_num_intervals = num_intervals if requested_num_intervals == None else int(requested_num_intervals)
 
-                if interval == "d":
-                    start_time = end_time - timedelta(days=num_intervals - 1)
-                elif interval == "w":
-                    start_time = end_time - timedelta(weeks=num_intervals - 1)
-                    
             else:
-                return generate_error_response(400, "numIntervals must be passed in for interval type d or w")
+                response = {"interval": "d", "halalCounts": [0] * 7, "haramCounts": [0] * 7}
+                return generate_success_response(response)
 
-            halal_counts = [None] * num_intervals
-            haram_counts = [None] * num_intervals
+            halal_counts = [0] * num_intervals
+            haram_counts = [0] * num_intervals
+            user_vote_map = {}
 
-            cur.execute('''
-                select utv.voteTime, utv.halalPoints, utv.haramPoints from UserTopicVotes utv
-                inner join 
-                    (
-                        select 
-                        max(voteTime) max_vote_time
-                        from UserTopicVotes
-                        where voteTime >= %(startTime)s and topicTitle=%(topicTitle)s
-                        group by date(`voteTime`)
-                    ) as utv_maxes
-                on utv.voteTime = utv_maxes.max_vote_time
-            ''', {'startTime': start_time, 'topicTitle': topic_title})
-            conn.commit()
-            vote_results = cur.fetchall()
+            for vote_record in vote_history:
+                username = vote_record["username"]
+                vote = vote_record["vote"]
+                vote_time = vote_record["voteTime"]
+                vote_time_floor = get_time_floor(vote_time)
 
-            if vote_results:
-                for vote_result in vote_results:
-                    vote_time = vote_result["voteTime"]
-                    vote_time_floor = get_time_floor(vote_time)
-                    halal_points = vote_result["halalPoints"]
-                    haram_points = vote_result["haramPoints"]
+                vote_time_diff = (end_time - vote_time_floor).days if interval_used == "d" else (end_time - vote_time_floor).days // 7
 
-                    vote_time_diff = (end_time - vote_time_floor).days if interval == "d" else (end_time - vote_time_floor).days // 7
+                current_halal_count = halal_counts[num_intervals - 1 - vote_time_diff]
+                current_haram_count = haram_counts[num_intervals - 1 - vote_time_diff]
 
-                    current_halal_count = halal_counts[num_intervals - 1 - vote_time_diff]
-                    current_haram_count = haram_counts[num_intervals - 1 - vote_time_diff]
+                if username in user_vote_map:
+                    prev_vote = user_vote_map[username]
 
-                    if current_halal_count == None or current_halal_count["time"] < vote_time:
-                        halal_counts[num_intervals - 1 - vote_time_diff] = {"points": halal_points, "time": vote_time}
+                    if prev_vote < 0:
+                        current_haram_count -= 1
+                    elif prev_vote > 0:
+                        current_halal_count -= 1
                     
-                    if current_haram_count == None or current_haram_count["time"] < vote_time:
-                        haram_counts[num_intervals - 1 - vote_time_diff] = {"points": haram_points, "time": vote_time}
-            
-            last_halal_points = 0
-            last_haram_points = 0
-
-            cur.execute('''
-                select halalPoints, haramPoints from UserTopicVotes
-                where topicTitle = %(topicTitle)s and voteTime < %(startTime)s
-                ORDER BY voteTime DESC LIMIT 1
-            ''', {'topicTitle': topic_title, 'startTime': start_time})
-            conn.commit()
-            initial_count_results = cur.fetchone()
-
-            if initial_count_results:
-                last_halal_points = initial_count_results["halalPoints"]
-                last_haram_points = initial_count_results["haramPoints"]
-
-            for i in range(num_intervals):
-                if halal_counts[i] == None:
-                    halal_counts[i] = last_halal_points
-                else:
-                    halal_counts[i] = halal_counts[i]["points"]
-                    last_halal_points = halal_counts[i]
+                if vote < 0:
+                    current_haram_count += 1
+                elif vote > 0:
+                    current_halal_count += 1
                 
-                if haram_counts[i] == None:
-                    haram_counts[i] = last_haram_points
-                else:
-                    haram_counts[i] = haram_counts[i]["points"]
-                    last_haram_points = haram_counts[i]
+                halal_counts[num_intervals - 1 - vote_time_diff] = current_halal_count
+                haram_counts[num_intervals - 1 - vote_time_diff] = current_haram_count
+                user_vote_map[username] = vote
 
-            return generate_success_response({"interval": interval, "halalCounts": halal_counts, "haramCounts": haram_counts})
+            total_halal_points = 0
+            total_haram_points = 0
+
+            for index, halal_count in enumerate(halal_counts):
+                if halal_count == None:
+                    halal_counts[index] = total_halal_points
+                else:
+                    halal_counts[index] += total_halal_points
+                total_halal_points = halal_counts[index]
+
+            for index, haram_count in enumerate(haram_counts):
+                if haram_count == None:
+                    haram_counts[index] = total_haram_points
+                else:
+                    haram_counts[index] += total_haram_points
+                total_haram_points = haram_counts[index]
+
+            if interval == "d" or interval == "w":
+                if requested_num_intervals > num_intervals:
+                    halal_counts = ([0] * (requested_num_intervals - num_intervals)) + halal_counts
+                    haram_counts = ([0] * (requested_num_intervals - num_intervals)) + haram_counts
+                elif requested_num_intervals < num_intervals:
+                    halal_counts = halal_counts[num_intervals - requested_num_intervals : num_intervals]
+                    haram_counts = haram_counts[num_intervals - requested_num_intervals : num_intervals]
+            
+            return generate_success_response({"interval": interval_used, "halalCounts": halal_counts, "haramCounts": haram_counts})
 
     except Exception as e:
         return generate_error_response(500, str(e))
--- topics_service/vote_topic.py
+++ topics_service/vote_topic.py
@@ -22,110 +22,97 @@ def vote_topic(data: dataType, conn, logger):
     # Access DB
     try:
         with conn.cursor() as cur:
-            prev_vote, prev_vote_time, latitude, longitude, halal_points, haram_points = (None, None, None, None, 0, 0)
-
-            cur.execute(
-                '''
-                    select vote, voteTime from UserTopicVotes
-                    where UserTopicVotes.username = %(username)s and UserTopicVotes.topicTitle = %(topicTitle)s and UserTopicVotes.current = true
-                ''',
-                {'username': username, 'topicTitle': topic_title}
-            )
-            vote_results = cur.fetchone()
-
-            if vote_results:
-                prev_vote, prev_vote_time = vote_results
-
-            cur.execute(
-                '''
-                    select lastLatitude, lastLongitude from Users
-                    where username = %(username)s
-                ''',
-                {'username': username}
-            )
-            location_results = cur.fetchone()
-
-            if location_results:
-                latitude, longitude = location_results
-
-            rows_affected = 0
-            # Update Topics table
-            if prev_vote != None and prev_vote != 0:
-                if prev_vote != vote:
-                    prev_vote_field = 'halalPoints' if prev_vote > 0 else 'haramPoints'
-                    vote_field = 'halalPoints' if vote > 0 else 'haramPoints'
-
-                    if vote == 0:
-                        query_set_section = prev_vote_field + " = " + prev_vote_field + " - 1, numVotes = numVotes - 1"
-                        query_string, query_params = createSetTopicsQuery(query_set_section, topic_title)
-                    else:
-                        query_set_section = vote_field + " = " + vote_field + " + 1, " + prev_vote_field + " = " + prev_vote_field + " - 1"
-                        query_string, query_params = createSetTopicsQuery(query_set_section, topic_title)
-                    
-                    rows_affected = cur.execute(query_string, query_params)
-                    conn.commit()
-
-                    if rows_affected != 0:
-                        query_string, query_params = creatGetVoteCountQuery(topic_title)
-                        cur.execute(query_string, query_params)
-                        update_results = cur.fetchone()
-
-                        if update_results:
-                            halal_points, haram_points = update_results
-
-            elif vote != 0:
-                vote_field = 'halalPoints' if vote > 0 else 'haramPoints'
-                query_set_section = vote_field + " = " + vote_field + " + 1, numVotes = numVotes + 1"
-                query_string, query_params = createSetTopicsQuery(query_set_section, topic_title)
-
-                rows_affected = cur.execute(query_string, query_params)
-                conn.commit()
-
-                if rows_affected != 0:
-                    query_string, query_params = creatGetVoteCountQuery(topic_title)
-                    cur.execute(query_string, query_params)
-                    update_results = cur.fetchone()
-
-                    if update_results:
-                        halal_points, haram_points = update_results
-
-            if rows_affected != 0:
-                # Update UserTopicVotes table
-                vote_bit = int(vote/abs(vote)) if vote != 0 else 0
-                current_time = generate_timestamp()
-
-                if prev_vote != None and prev_vote != vote_bit:
-                    cur.execute(
-                        '''
-                            update UserTopicVotes
-                            set current = false, removalTime = %(removalTime)s
-                            where username = %(username)s and topicTitle = %(topicTitle)s and current = true
-                        ''',
-                        {'removalTime': current_time,'username': username, 'topicTitle': topic_title}
-                    )
+            try:
+                prev_vote, prev_vote_time, latitude, longitude, halal_points, haram_points = (None, None, None, None, 0, 0)
 
                 cur.execute(
                     '''
-                        insert into UserTopicVotes (username, topicTitle, vote, voteTime, latitude, longitude, halalPoints, haramPoints)
-                        values (%(username)s, %(topicTitle)s, %(vote)s, %(voteTime)s, %(latitude)s, %(longitude)s, %(halalPoints)s, %(haramPoints)s)
+                        select vote, voteTime from UserTopicVotes
+                        where UserTopicVotes.username = %(username)s and UserTopicVotes.topicTitle = %(topicTitle)s and UserTopicVotes.current = true
                     ''',
-                    {'username': username, 'topicTitle': topic_title, 'vote': vote_bit, 'voteTime': current_time, 'latitude':latitude, 'longitude': longitude, 'halalPoints': halal_points, 'haramPoints': haram_points}
+                    {'username': username, 'topicTitle': topic_title}
                 )
-                conn.commit()
-                
-                with conn.cursor(pymysql.cursors.DictCursor) as dict_cur:
-                    dict_cur.execute(
+                vote_results = cur.fetchone()
+
+                if vote_results:
+                    prev_vote, prev_vote_time = vote_results
+
+                cur.execute(
+                    '''
+                        select lastLatitude, lastLongitude from Users
+                        where username = %(username)s
+                    ''',
+                    {'username': username}
+                )
+                location_results = cur.fetchone()
+
+                if location_results:
+                    latitude, longitude = location_results
+
+                # Update Topics table
+                rows_affected = 0
+                if prev_vote != None and prev_vote != 0:
+                    if prev_vote != vote:
+                        prev_vote_field = 'halalPoints' if prev_vote > 0 else 'haramPoints'
+                        vote_field = 'halalPoints' if vote > 0 else 'haramPoints'
+
+                        if vote == 0:
+                            query_set_section = prev_vote_field + " = " + prev_vote_field + " - 1, numVotes = numVotes - 1"
+                            query_string, query_params = createSetTopicsQuery(query_set_section, topic_title)
+                        else:
+                            query_set_section = vote_field + " = " + vote_field + " + 1, " + prev_vote_field + " = " + prev_vote_field + " - 1"
+                            query_string, query_params = createSetTopicsQuery(query_set_section, topic_title)
+                        
+                        rows_affected = cur.execute(query_string, query_params)
+
+                elif vote != 0:
+                    vote_field = 'halalPoints' if vote > 0 else 'haramPoints'
+                    query_set_section = vote_field + " = " + vote_field + " + 1, numVotes = numVotes + 1"
+                    query_string, query_params = createSetTopicsQuery(query_set_section, topic_title)
+
+                    rows_affected = cur.execute(query_string, query_params)
+
+                if rows_affected != 0:
+                    # Update UserTopicVotes table
+                    vote_bit = int(vote/abs(vote)) if vote != 0 else 0
+                    current_time = generate_timestamp()
+
+                    if prev_vote != None and prev_vote != vote_bit:
+                        cur.execute(
+                            '''
+                                update UserTopicVotes
+                                set current = false, removalTime = %(removalTime)s
+                                where username = %(username)s and topicTitle = %(topicTitle)s and current = true
+                            ''',
+                            {'removalTime': current_time, 'username': username, 'topicTitle': topic_title}
+                        )
+
+                    cur.execute(
                         '''
-                            select * from Topics where topicTitle = %(topicTitle)s
+                            insert into UserTopicVotes (username, topicTitle, vote, voteTime, latitude, longitude)
+                            values (%(username)s, %(topicTitle)s, %(vote)s, %(voteTime)s, %(latitude)s, %(longitude)s)
                         ''',
-                        {'topicTitle': topic_title}
+                        {'username': username, 'topicTitle': topic_title, 'vote': vote_bit, 'voteTime': current_time, 'latitude':latitude, 'longitude': longitude}
                     )
+
                     conn.commit()
-                    result = dict_cur.fetchall()[0]
-                    return generate_success_response(result)
-            else:
-                return generate_success_response({'noUpdates': True})
                     
+                    with conn.cursor(pymysql.cursors.DictCursor) as dict_cur:
+                        dict_cur.execute(
+                            '''
+                                select * from Topics where topicTitle = %(topicTitle)s
+                            ''',
+                            {'topicTitle': topic_title}
+                        )
+                        result = dict_cur.fetchall()[0]
+                        return generate_success_response(result)
+
+                else:
+                    return generate_success_response({'noUpdates': True})
+
+            except Exception as e:
+                conn.rollback()
+                raise
 
     except Exception as e:
         return generate_error_response(500, str(e))
