59a470ee8c9c8966c9da10586f8d0538f423b9f6
AbdulBaseer Khan <baseer@AbdulBaseers-MacBook-Pro-2.local>
2021-03-27 16:26:44 -0400

    Removed conn.commit() in get_parent_depth in utils

--- comments_service/utils.py
+++ comments_service/utils.py
@@ -7,7 +7,6 @@ def parent_depth_found(parent_depth):
 def get_parent_depth(conn, parent_id: int):
     with conn.cursor() as cur:
         cur.execute('select depth from Comments where id=%(parentId)s', {'parentId': parent_id})
-        conn.commit()
         result = cur.fetchone()
 
         return result if not result else result[0]
